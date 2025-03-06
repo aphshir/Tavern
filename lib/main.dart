9c1701b2a19e8a9e6d3feb3713133de79edc5c55
hoff nicolas <nicolas.hoff6@etu.univ-lorraine.fr>
2025-03-06 11:55:44 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'dart:convert'; 
+  
+import 'package:flutter/material.dart'; 
+import 'package:flutter/services.dart' show rootBundle; 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const p1(s:1),
+    );
+  }
+}
+
+class p1 extends StatefulWidget{
+  const p1({super.key, required this.s});
+  
+  final int s;
+  
+  @override
+  State<p1> createState() => _p1a();
+}
+
+class _p1a extends State<p1>{
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        centerTitle: true,
+        title: SizedBox(width: 200, child:TextField(
+          decoration: InputDecoration(
+            hintText: "Search",
+            prefixIcon: Icon(Icons.search),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        )),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+        
+            Text("wow"),
+            Text("j'ai fait ça tout seul")
+          ],
+        ),
+      ),
+    );
+  }
+}
